27383eef6f710767ca800a2d7b667d1c83997641
Bibash Roy <bibhash@gmail.com>
2023-07-04 12:23:53 +0530

    left-section of banner complete

---
+++ s
@@ -0,0 +1,29 @@
+[33mcommit 412ee6099bac490b85c2c7d6c3b959d63b4a0288[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Bibash Roy <bibhash@gmail.com>
+Date:   Tue Jul 4 11:42:05 2023 +0530
+
+    complete typescript
+
+[33mcommit 67a9bebd4c6cc148b904dd6c503ccd10b8f297fa[m
+Author: Bibash Roy <bibhash@gmail.com>
+Date:   Tue Jul 4 08:52:02 2023 +0530
+
+    complete 20minutes
+
+[33mcommit 84ca804a563ccd9a62123ffefbd60544bfe4c5c3[m
+Author: Bibash Roy <bibhash@gmail.com>
+Date:   Tue Jul 4 07:56:54 2023 +0530
+
+    at 8min and 10s
+
+[33mcommit 24c709096748131bfae0b29a31a1a19b1881c40b[m
+Author: Bibash Roy <bibhash@gmail.com>
+Date:   Tue Jul 4 07:55:31 2023 +0530
+
+    at
+
+[33mcommit 972400933cd0dcc78b50e99439efa159f1f1f146[m
+Author: Bibash Roy <bibhash@gmail.com>
+Date:   Tue Jul 4 07:27:13 2023 +0530
+
+    Initialize project using Create React App
--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,41 +1,16 @@
 import React from 'react'
-import {useTypewriter, Cursor} from "react-simple-typewriter"
+import LeftBanner from './LeftBanner';
+
 const Banner = () => {
 
-  const [text] = useTypewriter({
-    words:["Professional Coder." , "Full Stack Developer." , "UI Designer"],
-    Loop:true,
-    typeSpeed:20,
-    deleteSpeed:10,
-    delaySpeed:2000
-  })
+ 
 
   return (
     <section id="home" className='w-full py-20 flex items-center border-b-[1px] border-b-black font-titleFont'>
-      <div className="w-1/2">
-        <div className= "flex flex-col gap-5">
-            <h4 className="text-lg font-normal">
-                WELCOME TO MY WORLD
-            </h4>
-            <h1 className = "text-6xl font-bold text-white" > 
-              {" "}
-              Hi, I'm 
-              <span className="text-designColor capitalize"> Abhishek </span>
-            </h1>
-            <h2 className = "text-4xl font-bold text-white"> a <span> {text} </span>
-            <Cursor
-              cursorBlinking="false"
-              cursorStyle = " "
-              cursorColor = "#ffo14f"
-            >
-
-            </Cursor>
-            </h2>
-            <p className="text-base font-bodyFont leading-6 tracking-wide">
-              I use animation as a third dimensioon by which to simplify exprerienexs and kuiding thro each and every interaction. I'm not adding motion just to spruce things up , but doing it in ways that.
-            </p>
-        </div>
-      </div>
+    {/* LeftSection */}
+     
+      <LeftBanner />
+      {/* Right section  */}
       <div className = "w-1/2">
 
       </div>
